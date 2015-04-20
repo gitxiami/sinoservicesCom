d7b526e36c3bea8c3944f9cbdd01987ad607dcf8
Jerry <Jerry.Su@sinoservices.com>
2015-04-20 16:52:07 +0800

    1111

--- SinoservicesCommon/src/com/sinoservices/common/MainActivity.java
+++ SinoservicesCommon/src/com/sinoservices/common/MainActivity.java
@@ -1,7 +1,7 @@
 package com.sinoservices.common;
 import android.app.Activity;
 import android.os.Bundle;
-//this is first update
+//this is first update111
 public class MainActivity extends Activity {
 
 	@Override
